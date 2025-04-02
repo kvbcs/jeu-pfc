e21c68a668fe0bf805c346ca2d1a925dd0a9448b
Chr1stopherPEREZ <christopher11perez@hotmail.com>
2025-04-02 11:56:16 +0200

    Recuperation

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 //faire un prompt pfc joueur
 let playerChoice = prompt("Choisir (pierre/feuille/ciseaux)").toLowerCase();
 console.log(playerChoice);
-while (playerChoice !== "pierre" && playerChoice !== "feuille" && playerChoice !== "ciseaux") {
-    playerChoice = prompt("Entrez pierre, feuille ou ciseaux:").toLowerCase();
-  }
+while (
+  playerChoice !== "pierre" &&
+  playerChoice !== "feuille" &&
+  playerChoice !== "ciseaux"
+) {
+  playerChoice = prompt("Entrez pierre, feuille ou ciseaux:").toLowerCase();
+}
 
 //math random pfc
-let pfc0 = "pierre"
-let pfc1 = "feuille"
-let pfc2 = "ciseaux"
+let pfc0 = "pierre";
+let pfc1 = "feuille";
+let pfc2 = "ciseaux";
 
-const getRandomPfc = (max) =>{
-    return Math.floor(Math.random() * max);
-
-}
-let computerChoice = getRandomPfc(3)
+const getRandomPfc = (max) => {
+  return Math.floor(Math.random() * max);
+};
+let computerChoice = getRandomPfc(3);
 console.log(computerChoice);
 
 //alert choix ordinateur
-alert(computerChoice)
+alert(computerChoice);
 //fonction compare les 2
 
 // const comparePfc = () =>{
-    
+
 // }
 //égalité = les 2 gagnent, alert montre résultats
 //rejouer = comfirm
