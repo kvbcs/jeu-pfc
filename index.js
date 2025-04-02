327bd5e65e162402cdf1ebb761d2f6c4493e583b
Kyllian <kyllianvibancos2@gmail.com>
2025-04-02 10:10:54 +0200

    added instructions

---
+++ index.js
@@ -0,0 +1,8 @@
+//faire un prompt pfc joueur
+//math random pfc
+//alert choix ordinateur
+//fonction compare les 2
+//égalité = les 2 gagnent, alert montre résultats
+//rejouer = comfirm
+//gérer cas invalides (isnan)
+//bonus : compter nbre victoires
